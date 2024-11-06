5a1daefb3fa7e74ac9673e63e1f823142957d937
paulhunter <p.hunter.eng@gmail.com>
2024-11-06 01:19:10 -0800

    player acceleration, max speed, travel wrapping on screen

--- constants.py
+++ constants.py
@@ -9,3 +9,6 @@ ASTEROID_MAX_RADIUS = ASTEROID_KINDS * ASTEROID_MIN_RADIUS # pixels
 
 PLAYER_RADIUS = 20 # pixels
 PLAYER_TURN_SPEED = 300 # degrees per second
+PLAYER_MAX_SPEED = 200 # pixels per second
+PLAYER_ACCELERATION = 500 # pixels per second per second
+
--- player.py
+++ player.py
@@ -8,7 +8,12 @@ Note - the collision box will be a circle of PLAYER_RADIUS to simplify the math
 
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS \
+                    , PLAYER_TURN_SPEED \
+                    , PLAYER_ACCELERATION \
+                    , PLAYER_MAX_SPEED \
+                    , SCREEN_WIDTH \
+                    , SCREEN_HEIGHT
 
 class Player(CircleShape):
 
@@ -18,11 +23,13 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         
 
-    def triangle(self):
+    def forward(self):
         # y-axis is negative going up, positive going down
         # forward is north vector rotated by current rotation
-        forward = pygame.Vector2(0,-1).rotate(self.rotation)
-        # 
+        return pygame.Vector2(0,-1).rotate(self.rotation)
+
+    def triangle(self):
+        forward = self.forward()
         right = pygame.Vector2(0,1).rotate(self.rotation + 90) * self.radius / 1.5
 
         a = self.position + (forward * self.radius)
@@ -33,6 +40,13 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation = (self.rotation + (PLAYER_TURN_SPEED * dt)) % 360
+
+    def accel(self, dt):
+        forward = self.forward()
+        newVelocity = self.velocity + (forward * dt * PLAYER_ACCELERATION)
+        if (newVelocity.length() > PLAYER_MAX_SPEED):
+            newVelocity.scale_to_length(PLAYER_MAX_SPEED)
+        self.velocity = newVelocity
 
 # circleshape overrides
 
@@ -48,5 +62,25 @@ class Player(CircleShape):
         
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
+
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.accel(dt)
+
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.accel(-dt)
+
+        newPos = self.position + (self.velocity * dt);
+        # Bound the player to the screen
+        # Wrap around if they have traveled too far
+        if (newPos.x < 0):
+            newPos.update(newPos.x + SCREEN_WIDTH, newPos.y)
+        if (newPos.x > SCREEN_WIDTH):
+            newPos.update(newPos.x - SCREEN_WIDTH, newPos.y)
+        if (newPos.y < SCREEN_HEIGHT):
+            newPos.update(newPos.x, newPos.y + SCREEN_HEIGHT)
+        if (newPos.y > SCREEN_HEIGHT):
+            newPos.update(newPos.x, newPos.y - SCREEN_HEIGHT)
+
+        self.position = newPos;
 
 
