c9a3c1279f3c9904311d95824f5736add4f24f6e
paulhunter <p.hunter.eng@gmail.com>
2025-07-30 12:58:56 -0700

    stub - soundmanager

---
+++ soundmanager.py
@@ -0,0 +1,22 @@
+"""soundmanager.py
+Defines the Sound Manager, a helper for loading audio files, initializing the 
+pygame mixer, and playing sounds.
+
+Singleton
+"""
+
+class SoundManager:
+
+    def __init__(self):
+        pass
+
+    def load_sound_library():
+        pass
+
+    def unload_sound_library():
+        pass
+
+    def play_sound(name):
+        """Plays a given sound if available in the sound library."""
+        pass
+    
