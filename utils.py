8cd5110320f31113e4043a6bf1ed31fb8b05156c
paulhunter <p.hunter.eng@gmail.com>
2025-08-17 23:57:44 -0700

    utils - notes

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import random
 from pygame import Vector2
 
 class Utils:
+    """Utils
+    General utilities and helpers, a catch-all for shared code.
+    """
 
     @staticmethod
     def random_vector2(length = 1):
