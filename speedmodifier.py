83892d65f1d14cee1383cd6deca573e8feaf0f8b
paulhunter <p.hunter.eng@gmail.com>
2025-08-23 15:17:33 -0700

    speedmodifier.py style fixes

--- speedmodifier.py
+++ speedmodifier.py
@@ -1,9 +1,15 @@
+"""speedmodifier
+Defines the SpeedModifier class
+"""
 import pygame
 
 from modifierbase import ModifierBase
 from constants import PLAYER_ACCELERATION
 
 class SpeedModifier(ModifierBase):
+    """SpeedModifier
+    Ship modifier which increase the acceleration of the ship
+    """
     # def __init__(self, x, y, velocity = None): - No override.
     # def update(self, state, dt): - No override.
 
@@ -16,9 +22,9 @@ class SpeedModifier(ModifierBase):
 
     def draw(self, screen):
         pygame.draw.circle(screen, "green", self.position, self.radius, 2)
-        pygame.draw.line(screen, "green", 
-            self.position + (self.radius * 0.7 * pygame.Vector2(-1,-1)), 
-            self.position, 2)        
-        pygame.draw.line(screen, "green", 
-            self.position + (self.radius * 0.7 * pygame.Vector2(-1,1)), 
+        pygame.draw.line(screen, "green",
+            self.position + (self.radius * 0.7 * pygame.Vector2(-1, -1)),
+            self.position, 2)
+        pygame.draw.line(screen, "green",
+            self.position + (self.radius * 0.7 * pygame.Vector2(-1, 1)),
             self.position, 2)
