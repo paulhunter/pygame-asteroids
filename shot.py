f9c23504358cafe503d2fa31c820f0e3b22cf624
paulhunter <p.hunter.eng@gmail.com>
2025-08-27 13:09:50 -0700

    shot - tidy up

--- shot.py
+++ shot.py
@@ -1,21 +1,24 @@
+"""shot.py
+Defines the Shot class
+"""
+
 import pygame
 from circleshape import CircleShape
-from constants import SCREEN_WIDTH \
-                    , SCREEN_HEIGHT \
-                    , SHOT_RADIUS \
-                    , SHOT_COLOR
+from constants import SHOT_COLOR
 
 class Shot(CircleShape):
+    """Shot
+    A projectile most often fired from a ship.
+    """
     containers = None
 
 
     def __init__(self, x, y, radius, player):
         self.player = player
         super().__init__(x, y, radius)
-    
+
 
 # CircleShape overrides
-
     def draw(self, screen):
         pygame.draw.circle(screen, SHOT_COLOR, self.position, self.radius, 2)
 
